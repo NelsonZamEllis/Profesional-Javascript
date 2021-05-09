ff59e395bfcc877d0df44f59f694816ff6104422
“NelsonEllis” <nelson_905@hotmail.com>
2021-05-09 03:08:19 -0400

    hasta la clase de Service Workers

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,27 @@
-  import MediaPlayer from './MediaPlayer.js';
-  import AutoPlay from './plugins/AutoPlay.js';
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
-  const video = document.querySelector('video');
-  const player = new MediaPlayer({ el: video, plugins: [
-    new AutoPlay()
-  ] });
+const video = document.querySelector('video');
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
 
-  const button = document.querySelector('.button');
-  button.onclick = () => player.togglePlay();
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
 
-  const button2 = document.querySelector('.button2');
-  button2.onclick = () => player.unmute();
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
